715dcad63e5a5bbaebc12493a1422ee9a8751694
Luis Martinez (Sullen) <luis.d.sullen@outlook.com>
2024-02-17 09:58:17 -0600

    add Navbar

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+import EyeLogo from "../assets/Eye.svg";
+
+const Navbar = () => {
+  return (
+    <header className="flex gap-1 bg-gray-200 ">
+      <img src={EyeLogo} alt="Logo" className="size-9 m-2"/>
+      <nav className="flex gap-5">
+        <NavLink to='/' className="self-center rounded-lg p-2 hover:bg-gray-700 hover:text-white ease-in-out duration-200">Home</NavLink>
+        <NavLink to='/form' className="self-center rounded-lg p-2 hover:bg-gray-700 hover:text-white ease-in-out duration-200">Form</NavLink>             
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import RouterApp from "./router/RouterApp.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
+import Navbar from "./layout/Navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
+      <Navbar />
       <RouterApp />
     </Provider>
   </BrowserRouter>
